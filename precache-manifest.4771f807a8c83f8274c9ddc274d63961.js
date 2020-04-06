e1277c80f2bfd0f820cb48c77fbea50717dfeb72
Asam Shah <asamshah@gmail.com>
2020-04-06 13:32:52 +0100

    Updates

---
+++ precache-manifest.4771f807a8c83f8274c9ddc274d63961.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbd88b69d85b117865a78ba245723b89",
+    "url": "/todoapp/index.html"
+  },
+  {
+    "revision": "108196ef01e2a5e32219",
+    "url": "/todoapp/static/css/main.becefb9a.chunk.css"
+  },
+  {
+    "revision": "cd20965d169ab9accaca",
+    "url": "/todoapp/static/js/2.dccaa3e1.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/todoapp/static/js/2.dccaa3e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "108196ef01e2a5e32219",
+    "url": "/todoapp/static/js/main.b27592e6.chunk.js"
+  },
+  {
+    "revision": "8a9fa5bddf7520def859",
+    "url": "/todoapp/static/js/runtime-main.546fbf49.js"
+  }
+]);
